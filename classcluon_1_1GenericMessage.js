ae175810b1091d5bef407a59457ca5452d56130b
Christian Berger <christian.berger@gu.se>
2017-12-20 02:26:04 +0100

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- classcluon_1_1GenericMessage.js
+++
@@ -1,8 +0,0 @@
-var classcluon_1_1GenericMessage =
-[
-    [ "GenericMessage", "classcluon_1_1GenericMessage.html#a8f19dc27bcf57cef004880aebd7029e4", null ],
-    [ "GenericMessage", "classcluon_1_1GenericMessage.html#ad60c907d8712c82aa9916a2033b77a6f", null ],
-    [ "accept", "classcluon_1_1GenericMessage.html#a504ed40e4265fff90673730e9e388cdc", null ],
-    [ "accept", "classcluon_1_1GenericMessage.html#ad675a2c7c577ea3473c244edb1100a31", null ],
-    [ "setMetaMessage", "classcluon_1_1GenericMessage.html#a5aef8baab3d6a944792c74b96c839a54", null ]
-];
