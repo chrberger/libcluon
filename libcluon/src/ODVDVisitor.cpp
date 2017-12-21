80afc2772f64d8f415d9a04a8e6476570e463e5f
Christian Berger <christian.berger@gu.se>
2017-12-21 05:30:02 +0100

    * Added message to ODVD message specication extraction
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ libcluon/src/ODVDVisitor.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2017  Christian Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cluon/ODVDVisitor.hpp"
+
+#include <sstream>
+
+namespace cluon {
+
+std::string ODVDVisitor::messageSpecification() const noexcept {
+    std::stringstream tmp;
+    for (const auto &e : m_forwardDeclarations) {
+        tmp << e;
+    }
+    tmp << m_buffer.str();
+
+    const std::string retVal{tmp.str()};
+    return retVal;
+}
+
+void ODVDVisitor::preVisit(uint32_t id, const std::string &shortName, const std::string &longName) noexcept {
+    (void)shortName;
+    m_buffer << "message " << longName << " [ id = " << id << " ] {" << '\n';
+}
+
+void ODVDVisitor::postVisit() noexcept {
+    m_buffer << "}" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, bool &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "bool" << " " << name << " [ default = false, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, char &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "char" << " " << name << " [ default = '0', id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, int8_t &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "int8" << " " << name << " [ default = 0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, uint8_t &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "uint8" << " " << name << " [ default = 0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, int16_t &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "int16" << " " << name << " [ default = 0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, uint16_t &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "uint16" << " " << name << " [ default = 0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, int32_t &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "int32" << " " << name << " [ default = 0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, uint32_t &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "uint32" << " " << name << " [ default = 0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, int64_t &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "int64" << " " << name << " [ default = 0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, uint64_t &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "uint64" << " " << name << " [ default = 0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, float &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "float" << " " << name << " [ default = 0.0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, double &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "double" << " " << name << " [ default = 0.0, id = " << id << " ];" << '\n';
+}
+
+void ODVDVisitor::visit(uint32_t id, std::string &&typeName, std::string &&name, std::string &v) noexcept {
+    (void)typeName;
+    (void)v;
+    m_buffer << "    " << "string" << " " << name << " [ default = \"\", id = " << id << " ];" << '\n';
+}
+
+} // namespace cluon
---
+++ libcluon/testsuites/TestODVDVisitor.cpp
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2017  Christian Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "catch.hpp"
+#include <string>
+
+#include <iostream>
+
+#include "cluon/JSONVisitor.hpp"
+#include "cluon/ODVDVisitor.hpp"
+#include "cluon/GenericMessage.hpp"
+#include "cluon/MetaMessage.hpp"
+#include "cluon/MessageParser.hpp"
+#include "cluon/MessageToProtoEncoder.hpp"
+#include "cluon/MessageFromProtoDecoder.hpp"
+#include "cluon/cluonTestDataStructures.hpp"
+
+TEST_CASE("Testing MyTestMessage1.") {
+    testdata::MyTestMessage1 tmp;
+
+    tmp.attribute11(-9.123456f).attribute12(10.123456789);
+
+    REQUIRE(tmp.attribute1());
+    REQUIRE('c' == tmp.attribute2());
+    REQUIRE(-1 == tmp.attribute3());
+    REQUIRE(2 == tmp.attribute4());
+    REQUIRE(-3 == tmp.attribute5());
+    REQUIRE(4 == tmp.attribute6());
+    REQUIRE(-5 == tmp.attribute7());
+    REQUIRE(6 == tmp.attribute8());
+    REQUIRE(-7 == tmp.attribute9());
+    REQUIRE(8 == tmp.attribute10());
+    REQUIRE(-9.123456 == Approx(tmp.attribute11()));
+    REQUIRE(10.123456789 == Approx(tmp.attribute12()));
+    REQUIRE("Hello World" == tmp.attribute13());
+    REQUIRE("Hello Galaxy" == tmp.attribute14());
+
+    cluon::MessageAsProtoEncoder protoEncoder;
+    tmp.accept(protoEncoder);
+    const std::string protoEncoded{protoEncoder.encodedData()};
+
+    cluon::ODVDVisitor odvdVisitor;
+    tmp.accept(odvdVisitor);
+
+    const char *msg = R"(message testdata.MyTestMessage1 [ id = 30001 ] {
+    bool attribute1 [ default = false, id = 1 ];
+    char attribute2 [ default = '0', id = 2 ];
+    int8 attribute3 [ default = 0, id = 3 ];
+    uint8 attribute4 [ default = 0, id = 4 ];
+    int16 attribute5 [ default = 0, id = 5 ];
+    uint16 attribute6 [ default = 0, id = 6 ];
+    int32 attribute7 [ default = 0, id = 7 ];
+    uint32 attribute8 [ default = 0, id = 8 ];
+    int64 attribute9 [ default = 0, id = 9 ];
+    uint64 attribute10 [ default = 0, id = 10 ];
+    float attribute11 [ default = 0.0, id = 11 ];
+    double attribute12 [ default = 0.0, id = 12 ];
+    string attribute13 [ default = "", id = 13 ];
+    string attribute14 [ default = "", id = 14 ];
+}
+)";
+
+    const std::string generatedMessageSpecification{odvdVisitor.messageSpecification()};
+    REQUIRE(std::string(msg) == generatedMessageSpecification);
+
+    cluon::MessageParser mp;
+    auto retVal = mp.parse(generatedMessageSpecification);
+    REQUIRE(cluon::MessageParser::MessageParserErrorCodes::NO_ERROR == retVal.second);
+    auto listOfMetaMessages = retVal.first;
+    REQUIRE(1 == listOfMetaMessages.size());
+
+    cluon::MessageFromProtoDecoder protoDecoder;
+    std::stringstream sstr{protoEncoded};
+    protoDecoder.decodeFrom(sstr);
+
+    cluon::MetaMessage m = listOfMetaMessages.front();
+    cluon::GenericMessage gm;
+    gm.createFrom(m, listOfMetaMessages, protoDecoder);
+
+    cluon::JSONVisitor j;
+    gm.accept(j);
+
+    const char *JSON = R"({"attribute1":1,
+"attribute2":"c",
+"attribute3":-1,
+"attribute4":2,
+"attribute5":-3,
+"attribute6":4,
+"attribute7":-5,
+"attribute8":6,
+"attribute9":-7,
+"attribute10":8,
+"attribute11":-9.123456,
+"attribute12":10.123456789,
+"attribute13":"Hello World",
+"attribute14":"Hello Galaxy"})";
+
+    REQUIRE(std::string(JSON) == j.json());
+}
