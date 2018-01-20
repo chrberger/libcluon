cc009444365573f6d2a36002cb6082955f4caf84
Christian Berger <christian.berger@gu.se>
2018-01-20 08:37:03 +0100

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- structisTripletForwardVisitable_3_01cluon_1_1GenericMessage_01_4.js
+++
@@ -1,4 +0,0 @@
-var structisTripletForwardVisitable_3_01cluon_1_1GenericMessage_01_4 =
-[
-    [ "value", "structisTripletForwardVisitable_3_01cluon_1_1GenericMessage_01_4.html#a19902d99a0cce71941678d8c63b42711", null ]
-];
