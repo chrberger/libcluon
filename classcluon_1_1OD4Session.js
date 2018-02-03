347cbfe2225c08b7a6640d166b35b0dd4e7f1f55
Christian Berger <christian.berger@gu.se>
2018-02-03 23:38:19 +0100

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- classcluon_1_1OD4Session.js
+++
@@ -1,6 +0,0 @@
-var classcluon_1_1OD4Session =
-[
-    [ "OD4Session", "classcluon_1_1OD4Session.html#ad9d26426cf2714e105c27a23ce4a0f7a", null ],
-    [ "isRunning", "classcluon_1_1OD4Session.html#a1381dc975919e3ef0592e9bfa032c8df", null ],
-    [ "send", "classcluon_1_1OD4Session.html#a6ef9959ff7b3b39591304f047165d762", null ]
-];
