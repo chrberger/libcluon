6a131ea0c1f1f02b85561f7abd8953b722141b68
Christian Berger <christian.berger@gu.se>
2018-02-19 09:50:48 +0100

    Documentation for v0.0.48
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ Envelope_8hpp.js
@@ -0,0 +1,5 @@
+var Envelope_8hpp =
+[
+    [ "extractEnvelope", "Envelope_8hpp.html#aed0cd92bb770ffdcba8c63de7ad1e6ac", null ],
+    [ "extractMessage", "Envelope_8hpp.html#a5bc1a93d827cb64aa1b353924222a7c9", null ]
+];
---
+++ cluon_8hpp.js
@@ -0,0 +1,8 @@
+var cluon_8hpp =
+[
+    [ "LIBCLUON_API", "cluon_8hpp.html#a450521e9bcf01c91d8d213ee91e9b4ff", null ],
+    [ "getCommandlineArguments", "cluon_8hpp.html#a276317d2317901f48d8283de15bdd924", null ],
+    [ "ltrim", "cluon_8hpp.html#a145ef89f6ddd61c424b9f42216235acb", null ],
+    [ "rtrim", "cluon_8hpp.html#a65c2daf9dd52495aa215efa2f69faab3", null ],
+    [ "trim", "cluon_8hpp.html#ad92945fded51efb837e459ebfa9b805a", null ]
+];
