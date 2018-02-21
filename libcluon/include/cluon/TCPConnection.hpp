2e1f9f21b5b369cf8280a93c850ca72ad1d7ba0a
Christian Berger <christian.berger@gu.se>
2018-02-21 20:57:08 +0100

    * TCP connection
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ libcluon/include/cluon/TCPConnection.hpp
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2018  Christian Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TCPCONNECTION_HPP
+#define TCPCONNECTION_HPP
+
+#include "cluon/cluon.hpp"
+
+// clang-format off
+#ifdef WIN32
+    #include <Winsock2.h> // for WSAStartUp
+    #include <Ws2def.h>   // for struct sockaddr_in
+    #include <ws2tcpip.h> // for SOCKET
+#else
+    #include <netinet/in.h>
+#endif
+// clang-format on
+
+#include <cstdint>
+#include <atomic>
+#include <chrono>
+#include <functional>
+#include <string>
+#include <thread>
+
+namespace cluon {
+/**
+To exchange data via TCP, simply include the header
+`#include <cluon/TCPConnection.hpp>`.
+
+Next, create an instance of class `cluon::TCPConnection` as follows:
+`cluon::TCPConnection connection("127.0.0.1", 1234, newDataDelegate, connectionLostDelegate);`.
+The first parameter is of type `std::string` expecting a numerical IPv4 address,
+the second parameter specifies the TCP port, from which data shall be received
+from, the third paraemter is of type `std::function` that is called whenever
+new bytes are available to be processed, and the last parameter is of type
+`std::function` that is called when the connection is lost.
+
+The complete signature for the newDataDelegate function is
+`std::function<void(std::string &&, std::string &&, std::chrono::system_clock::time_point &&) noexcept>`:
+The first parameter contains the bytes that have been received, the second
+parameter containes the human-readable representation of the sender
+(X.Y.Z.W:ABCD), and the last parameter is the time stamp when the data has been
+received.
+
+The complete signature for the connectionLostDelegate function is
+`std::function<void() noexcept>`.
+
+To finally send data, simply call the method `send` supplying the data to be
+sent: `connection.send(std::move("Hello World!")`. Please note that the data is
+supplied using the _move_-semantics. The method `send` returns a
+`std::pair<ssize_t, int32_t>` where the first element returns the size of the
+successfully sent bytes and the second element contains the error code in case
+the transmission of the data failed.
+
+An example using a C++ lambda expression would look as follows:
+
+\code{.cpp}
+cluon::TCPConnection connection("127.0.0.1", 1234,
+    [](std::string &&data, std::string &&sender, std::chrono::system_clock::time_point &&ts) noexcept {
+        const auto timestamp(std::chrono::system_clock::to_time_t(ts));
+        std::cout << "Received " << data.size() << " bytes"
+                  << " from " << sender
+                  << " at " << std::put_time(std::localtime(&timestamp), "%Y-%m-%d %X")
+                  << ", containing '" << data << "'." << std::endl;
+    },
+    [](){ std::cout << "Connection lost." << std::endl; });
+
+std::pair<ssize_t, int32_t> retVal = connection.send(std::move("Hello World!"));
+\endcode
+
+After creating an instance of class `cluon::TCPConnection`, it is immediately
+activated and concurrently waiting for data in a separate thread. To check
+whether the instance was created successfully and running, the method
+`isRunning()` should be called.
+*/
+class LIBCLUON_API TCPConnection {
+   private:
+    TCPConnection(const TCPConnection &) = delete;
+    TCPConnection(TCPConnection &&)      = delete;
+    TCPConnection &operator=(const TCPConnection &) = delete;
+    TCPConnection &operator=(TCPConnection &&) = delete;
+
+   public:
+    /**
+     * Constructor.
+     *
+     * @param address Numerical IPv4 address to receive UDP packets from.
+     * @param port Port to receive UDP packets from.
+     * @param newDataDelegate Functional (noexcept) to handle received bytes; parameters are received data, timestamp.
+     * @param connectionLostDelegate Functional (noexcept) to handle a lost connection.
+     */
+    TCPConnection(const std::string &address,
+                uint16_t port,
+                std::function<void(std::string &&, std::chrono::system_clock::time_point &&)> newDataDelegate,
+                std::function<void()> connectionLostDelegate) noexcept;
+    ~TCPConnection() noexcept;
+
+    /**
+     * @return true if the TCPConnection could successfully be created and is able to receive data.
+     */
+    bool isRunning() const noexcept;
+
+    /**
+     * Send a given string.
+     *
+     * @param data Data to send.
+     * @return Pair: Number of bytes sent and errno.
+     */
+    std::pair<ssize_t, int32_t> send(std::string &&data) const noexcept;
+
+   private:
+    /**
+     * This method closes the socket.
+     *
+     * @param errorCode Error code that caused this closing.
+     */
+    void closeSocket(int errorCode) noexcept;
+    void readFromSocket() noexcept;
+
+   private:
+    int32_t m_socket{-1};
+    struct sockaddr_in m_address {};
+    std::atomic<bool> m_readFromSocketThreadRunning{false};
+    std::thread m_readFromSocketThread{};
+    std::function<void(std::string &&, std::chrono::system_clock::time_point)> m_newDataDelegate{};
+    std::function<void()> m_connectionLostDelegate{};
+};
+} // namespace cluon
+
+#endif
---
+++ libcluon/src/TCPConnection.cpp
@@ -0,0 +1,243 @@
+/*
+ * Copyright (C) 2018  Christian Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cluon/TCPConnection.hpp"
+#include "cluon/cluonDataStructures.hpp"
+
+// clang-format off
+#ifdef WIN32
+    #include <errno.h>
+    #include <iostream>
+#else
+    #include <arpa/inet.h>
+    #include <sys/ioctl.h>
+    #include <sys/socket.h>
+    #include <sys/types.h>
+    #include <unistd.h>
+#endif
+// clang-format on
+
+#include <cstring>
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+namespace cluon {
+
+TCPConnection::TCPConnection(const std::string &address,
+                            uint16_t port,
+                            std::function<void(std::string &&, std::chrono::system_clock::time_point &&)> newDataDelegate,
+                            std::function<void()> connectionLostDelegate) noexcept
+    : m_address()
+    , m_readFromSocketThread()
+    , m_newDataDelegate(std::move(newDataDelegate))
+    , m_connectionLostDelegate(std::move(connectionLostDelegate)) {
+    // Decompose given address string to check validity with numerical IPv4 address.
+    std::string tmp{address};
+    std::replace(tmp.begin(), tmp.end(), '.', ' ');
+    std::istringstream sstr{tmp};
+    std::vector<int> addressTokens{std::istream_iterator<int>(sstr), std::istream_iterator<int>()};
+
+    if ((!addressTokens.empty()) && (4 == addressTokens.size())
+        && !(std::end(addressTokens)
+             != std::find_if(addressTokens.begin(), addressTokens.end(), [](int a) { return (a < 0) || (a > 255); }))
+        && (0 < port)) {
+        // Check for valid IP address.
+        struct sockaddr_in tmpSocketAddress {};
+        const bool isValid = (0 < ::inet_pton(AF_INET, address.c_str(), &(tmpSocketAddress.sin_addr)));
+
+        std::memset(&m_address, 0, sizeof(m_address));
+        m_address.sin_addr.s_addr = ::inet_addr(address.c_str());
+        m_address.sin_family = AF_INET;
+        m_address.sin_port   = htons(port);
+
+#ifdef WIN32
+        // Load Winsock 2.2 DLL.
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cerr << "[cluon::TCPConnection] Error while calling WSAStartUp: " << WSAGetLastError() << std::endl;
+        }
+#endif
+
+        m_socket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+#ifdef WIN32
+        if (m_socket < 0) {
+            std::cerr << "[cluon::TCPConnection] Error while creating socket: " << WSAGetLastError() << std::endl;
+            WSACleanup();
+        }
+#endif
+
+        if (!(m_socket < 0)) {
+            auto retVal = ::connect(m_socket , reinterpret_cast<struct sockaddr *>(&m_address), sizeof(m_address));
+            if (0 > retVal) {
+#ifdef WIN32
+                auto errorCode = WSAGetLastError();
+#else
+                auto errorCode = errno;
+#endif
+                closeSocket(errorCode);
+            }
+            else {
+                // Constructing a thread could fail.
+                try {
+                    m_readFromSocketThread = std::thread(&TCPConnection::readFromSocket, this);
+
+                    // Let the operating system spawn the thread.
+                    using namespace std::literals::chrono_literals;
+                    do { std::this_thread::sleep_for(1ms); } while (!m_readFromSocketThreadRunning.load());
+                } catch (...) { closeSocket(ECHILD); }
+            }
+        }
+    }
+}
+
+TCPConnection::~TCPConnection() noexcept {
+    m_readFromSocketThreadRunning.store(false);
+
+    // Joining the thread could fail.
+    try {
+        if (m_readFromSocketThread.joinable()) {
+            m_readFromSocketThread.join();
+        }
+    } catch (...) {}
+
+    closeSocket(0);
+}
+
+void TCPConnection::closeSocket(int errorCode) noexcept {
+    if (0 != errorCode) {
+        std::cerr << "[cluon::TCPConnection] Failed to perform socket operation: ";
+#ifdef WIN32
+        std::cerr << errorCode << std::endl;
+#else
+        std::cerr << ::strerror(errorCode) << " (" << errorCode << ")" << std::endl;
+#endif
+    }
+
+    if (!(m_socket < 0)) {
+#ifdef WIN32
+        ::shutdown(m_socket, SD_BOTH);
+        ::closesocket(m_socket);
+        WSACleanup();
+#else
+        ::shutdown(m_socket, SHUT_RDWR); // Disallow further read/write operations.
+        ::close(m_socket);
+#endif
+    }
+    m_socket = -1;
+}
+
+bool TCPConnection::isRunning() const noexcept {
+    return m_readFromSocketThreadRunning.load();
+}
+
+std::pair<ssize_t, int32_t> TCPConnection::send(std::string &&data) const noexcept {
+    if (-1 == m_socket) {
+        return {-1, EBADF};
+    }
+
+    if (data.empty()) {
+        return {0, 0};
+    }
+
+    if (!m_readFromSocketThreadRunning.load()) {
+        m_connectionLostDelegate();
+        return {-1, ENOTCONN};
+    }
+
+    constexpr uint16_t MAX_LENGTH{65535};
+    if (MAX_LENGTH < data.size()) {
+        return {-1, E2BIG};
+    }
+
+//    std::lock_guard<std::mutex> lck(m_socketMutex);
+    ssize_t bytesSent = ::send(m_socket, data.c_str(), data.length(), 0);
+    return {bytesSent, (0 > bytesSent ? errno : 0)};
+}
+
+void TCPConnection::readFromSocket() noexcept {
+    // Create buffer to store data from socket.
+    constexpr uint16_t MAX_LENGTH{65535};
+    std::array<char, MAX_LENGTH> buffer{};
+
+    // Define timeout for select system call.
+    struct timeval timeout {};
+    timeout.tv_sec  = 0;
+    timeout.tv_usec = 20 * 1000; // Check for new data with 50Hz.
+
+    // Define file descriptor set to watch for read operations.
+    fd_set setOfFiledescriptorsToReadFrom{};
+
+    ssize_t bytesRead{0};
+
+    // Sender address and port.
+    constexpr uint16_t MAX_ADDR_SIZE{1024};
+    std::array<char, MAX_ADDR_SIZE> remoteAddress{};
+
+    // Indicate to main thread that we are ready.
+    m_readFromSocketThreadRunning.store(true);
+
+    while (m_readFromSocketThreadRunning.load()) {
+        FD_ZERO(&setOfFiledescriptorsToReadFrom);
+        FD_SET(m_socket, &setOfFiledescriptorsToReadFrom);
+        ::select(m_socket + 1, &setOfFiledescriptorsToReadFrom, nullptr, nullptr, &timeout);
+        if (FD_ISSET(m_socket, &setOfFiledescriptorsToReadFrom)) {
+            bytesRead = ::recv(m_socket, buffer.data(), buffer.max_size(), 0);
+            if (0 > bytesRead) {
+                m_readFromSocketThreadRunning.store(false);
+                if (nullptr != m_connectionLostDelegate) {
+                    m_connectionLostDelegate();
+                }
+                break;
+            }
+            if ((0 < bytesRead) && (nullptr != m_newDataDelegate)) {
+#ifdef __linux__
+                std::chrono::system_clock::time_point timestamp;
+                struct timeval receivedTimeStamp {};
+                if (0 == ::ioctl(m_socket, SIOCGSTAMP, &receivedTimeStamp)) {
+                    // Transform struct timeval to C++ chrono.
+                    std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds> transformedTimePoint(
+                        std::chrono::microseconds(receivedTimeStamp.tv_sec * 1000000L + receivedTimeStamp.tv_usec));
+                    timestamp = std::chrono::time_point_cast<std::chrono::system_clock::duration>(transformedTimePoint);
+
+                    cluon::data::TimeStamp ts;
+                    ts.seconds(static_cast<int32_t>(receivedTimeStamp.tv_sec));
+                    ts.microseconds(static_cast<int32_t>(receivedTimeStamp.tv_usec));
+
+                } else {
+                    // In case the ioctl failed, fall back to chrono.
+                    timestamp = std::chrono::system_clock::now();
+                }
+#else
+                std::chrono::system_clock::time_point timestamp = std::chrono::system_clock::now();
+#endif
+                // Call newDataDelegate.
+                m_newDataDelegate(std::string(buffer.data(), static_cast<size_t>(bytesRead)), timestamp);
+            }
+        } else {
+            // Let the operating system yield other threads.
+            using namespace std::literals::chrono_literals;
+            std::this_thread::sleep_for(1ms);
+        }
+    }
+}
+} // namespace cluon
