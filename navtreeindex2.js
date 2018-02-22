bd04d8415b00235397e71a65a464ee52f3e0a409
Christian Berger <christian.berger@gu.se>
2018-02-22 20:42:20 +0100

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- navtreeindex2.js
+++
@@ -1,9 +0,0 @@
-var NAVTREEINDEX2 =
-{
-"namespaces.html":[0,0],
-"pages.html":[],
-"structisTripletForwardVisitable_3_01cluon_1_1GenericMessage_01_4.html":[1,0,1],
-"structisTripletForwardVisitable_3_01cluon_1_1GenericMessage_01_4.html#a19902d99a0cce71941678d8c63b42711":[1,0,1,0],
-"structisVisitable_3_01cluon_1_1GenericMessage_01_4.html":[1,0,2],
-"structisVisitable_3_01cluon_1_1GenericMessage_01_4.html#a95a7f89d20e16352e0063716b95147da":[1,0,2,0]
-};
