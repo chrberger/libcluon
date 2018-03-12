660274a29281231491ddf0790c7eb916847ae544
Christian Berger <christian.berger@gu.se>
2018-03-12 22:02:53 +0100

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- classcluon_1_1EnvelopeToJSON.js
+++
@@ -1,7 +0,0 @@
-var classcluon_1_1EnvelopeToJSON =
-[
-    [ "EnvelopeToJSON", "classcluon_1_1EnvelopeToJSON.html#a37e2546f545744c64e2d906f08ed00e9", null ],
-    [ "getJSONFromEnvelope", "classcluon_1_1EnvelopeToJSON.html#ad8fdcc5657ed873ab420438863ccd7c2", null ],
-    [ "getJSONFromProtoEncodedEnvelope", "classcluon_1_1EnvelopeToJSON.html#a6d08844cda9951152270a2b54e295558", null ],
-    [ "setMessageSpecification", "classcluon_1_1EnvelopeToJSON.html#af6e9d3d9d189bb0b1d62bd1677e87ae6", null ]
-];
