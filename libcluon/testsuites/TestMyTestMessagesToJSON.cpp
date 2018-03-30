10500b7baa927e3189e9b633e8a8ca7812005000
Christian Berger <christian.berger@gu.se>
2018-03-30 21:29:02 +0200

    * More tests
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- libcluon/testsuites/TestMyTestMessagesToJSON.cpp
+++ libcluon/testsuites/TestMyTestMessagesToJSON.cpp
@@ -263,4 +263,92 @@ TEST_CASE("Testing MyTestMessage4.") {
     REQUIRE("testing" == tmp2.attribute1());
 }
 
+TEST_CASE("Testing MyTestMessage5.") {
+    testdata::MyTestMessage5 tmp;
+
+    REQUIRE(1 == tmp.attribute1());
+    REQUIRE(-1 == tmp.attribute2());
+    REQUIRE(100 == tmp.attribute3());
+    REQUIRE(-100 == tmp.attribute4());
+    REQUIRE(10000 == tmp.attribute5());
+    REQUIRE(-10000 == tmp.attribute6());
+    REQUIRE(12345 == tmp.attribute7());
+    REQUIRE(-12345 == tmp.attribute8());
+    REQUIRE(-1.2345f == Approx(tmp.attribute9()));
+    REQUIRE(-10.2345 == Approx(tmp.attribute10()));
+    REQUIRE("Hello World!" == tmp.attribute11());
+
+    tmp.attribute1(3)
+        .attribute2(-3)
+        .attribute3(103)
+        .attribute4(-103)
+        .attribute5(10003)
+        .attribute6(-10003)
+        .attribute7(54321)
+        .attribute8(-74321)
+        .attribute9(-5.4321f)
+        .attribute10(-50.4321)
+        .attribute11("Hello cluon World!");
+
+    REQUIRE(3 == tmp.attribute1());
+    REQUIRE(-3 == tmp.attribute2());
+    REQUIRE(103 == tmp.attribute3());
+    REQUIRE(-103 == tmp.attribute4());
+    REQUIRE(10003 == tmp.attribute5());
+    REQUIRE(-10003 == tmp.attribute6());
+    REQUIRE(54321 == tmp.attribute7());
+    REQUIRE(-74321 == tmp.attribute8());
+    REQUIRE(-5.4321f == Approx(tmp.attribute9()));
+    REQUIRE(-50.4321 == Approx(tmp.attribute10()));
+    REQUIRE("Hello cluon World!" == tmp.attribute11());
+
+    cluon::ToJSONVisitor jsonEncoder;
+    tmp.accept(jsonEncoder);
+
+    const char *JSON = R"({"attribute1":3,
+"attribute2":-3,
+"attribute3":103,
+"attribute4":-103,
+"attribute5":10003,
+"attribute6":-10003,
+"attribute7":54321,
+"attribute8":-74321,
+"attribute9":-5.4321,
+"attribute10":-50.4321,
+"attribute11":"SGVsbG8gY2x1b24gV29ybGQh"})";
+
+    REQUIRE(std::string(JSON) == jsonEncoder.json());
+
+    std::stringstream sstr{jsonEncoder.json()};
+
+    cluon::FromJSONVisitor jsonDecoder;
+    jsonDecoder.decodeFrom(sstr);
+
+    testdata::MyTestMessage5 tmp2;
+    REQUIRE(1 == tmp2.attribute1());
+    REQUIRE(-1 == tmp2.attribute2());
+    REQUIRE(100 == tmp2.attribute3());
+    REQUIRE(-100 == tmp2.attribute4());
+    REQUIRE(10000 == tmp2.attribute5());
+    REQUIRE(-10000 == tmp2.attribute6());
+    REQUIRE(12345 == tmp2.attribute7());
+    REQUIRE(-12345 == tmp2.attribute8());
+    REQUIRE(-1.2345f == Approx(tmp2.attribute9()));
+    REQUIRE(-10.2345 == Approx(tmp2.attribute10()));
+    REQUIRE("Hello World!" == tmp2.attribute11());
+
+    tmp2.accept(jsonDecoder);
+
+    REQUIRE(tmp2.attribute1() == tmp.attribute1());
+    REQUIRE(tmp2.attribute2() == tmp.attribute2());
+    REQUIRE(tmp2.attribute3() == tmp.attribute3());
+    REQUIRE(tmp2.attribute4() == tmp.attribute4());
+    REQUIRE(tmp2.attribute5() == tmp.attribute5());
+    REQUIRE(tmp2.attribute6() == tmp.attribute6());
+    REQUIRE(tmp2.attribute7() == tmp.attribute7());
+    REQUIRE(tmp2.attribute8() == tmp.attribute8());
+    REQUIRE(tmp2.attribute9() == Approx(tmp.attribute9()));
+    REQUIRE(tmp2.attribute10() == Approx(tmp.attribute10()));
+    REQUIRE(tmp2.attribute11() == tmp.attribute11());
+}
 
