c857618380ef6e47167cefb3363c05ef0503fd01
Christian Berger <christian.berger@gu.se>
2018-03-31 22:45:59 +0200

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- classcluon_1_1MetaMessageToCPPTransformator.js
+++
@@ -1,8 +0,0 @@
-var classcluon_1_1MetaMessageToCPPTransformator =
-[
-    [ "MetaMessageToCPPTransformator", "classcluon_1_1MetaMessageToCPPTransformator.html#a73d30b2afe45c3fd24f4b1ca113bea07", null ],
-    [ "MetaMessageToCPPTransformator", "classcluon_1_1MetaMessageToCPPTransformator.html#ae7b1f8ccadbc2551a7c5b0efb336b71e", null ],
-    [ "contentHeader", "classcluon_1_1MetaMessageToCPPTransformator.html#a6b7fd0cb5b362553d04b1510b80b7435", null ],
-    [ "contentSource", "classcluon_1_1MetaMessageToCPPTransformator.html#a4cec52beafa6aacbd5bf6f8f976b6001", null ],
-    [ "visit", "classcluon_1_1MetaMessageToCPPTransformator.html#a8b2af4322c065bbd182f5b13e4f8b43b", null ]
-];
--- classcluon_1_1TCPConnection.js
+++
@@ -1,7 +0,0 @@
-var classcluon_1_1TCPConnection =
-[
-    [ "TCPConnection", "classcluon_1_1TCPConnection.html#a3b2f7093b0c25df9cd1ded2513fe57f1", null ],
-    [ "~TCPConnection", "classcluon_1_1TCPConnection.html#a1592d281a7b873046c1d22d3e17e00c8", null ],
-    [ "isRunning", "classcluon_1_1TCPConnection.html#a668b63d0a93c03fa7ead086d8fe80c66", null ],
-    [ "send", "classcluon_1_1TCPConnection.html#a5e7e72bd00d40f107846bbcdcf5eb07f", null ]
-];
