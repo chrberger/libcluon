3e6fdf21c0116e7ab81276fac986d4e56593aaca
Christian Berger <christian.berger@gu.se>
2018-04-07 20:18:48 +0200

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- classcluon_1_1EnvelopeConverter.js
+++
@@ -1,8 +0,0 @@
-var classcluon_1_1EnvelopeConverter =
-[
-    [ "EnvelopeConverter", "classcluon_1_1EnvelopeConverter.html#a5e8ece063bd077c98698288eb4d49b65", null ],
-    [ "getJSONFromEnvelope", "classcluon_1_1EnvelopeConverter.html#a8d7438e6f54c7d4034688634a19ece83", null ],
-    [ "getJSONFromProtoEncodedEnvelope", "classcluon_1_1EnvelopeConverter.html#adb3484e4e1336792ffb51d870e0f8028", null ],
-    [ "getProtoEncodedEnvelopeFromJSONWithoutTimeStamps", "classcluon_1_1EnvelopeConverter.html#a5057e162133dce051602af2cdf7eda2d", null ],
-    [ "setMessageSpecification", "classcluon_1_1EnvelopeConverter.html#af4879d33814e727df61273fdd4ed973b", null ]
-];
