b0769bce6a1466c99ccdecd235fbd3ba4b50165b
Christian Berger <christian.berger@gu.se>
2018-04-17 23:02:47 +0200

    Documentation for v0.0.73
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ classcluon_1_1Player.js
@@ -0,0 +1,13 @@
+var classcluon_1_1Player =
+[
+    [ "Player", "classcluon_1_1Player.html#a8d1bdbed62ad11eedc1962ea136def69", null ],
+    [ "~Player", "classcluon_1_1Player.html#a9adcc77ee028ff248b7e2cc9429a89bc", null ],
+    [ "getCorrectedDelay", "classcluon_1_1Player.html#af20bb25eb9e096163a256857901a0457", null ],
+    [ "getDelay", "classcluon_1_1Player.html#a6a6087825c23e49e3a8d5b2df61cf928", null ],
+    [ "getNextEnvelopeToBeReplayed", "classcluon_1_1Player.html#ad67d2152a3febb6ca11b23e116756dd3", null ],
+    [ "getTotalNumberOfEnvelopesInRecFile", "classcluon_1_1Player.html#ae7abf46f754f9878e88a527b5557b3a2", null ],
+    [ "hasMoreData", "classcluon_1_1Player.html#a354be5addf477e9eaac9fdffa684c71c", null ],
+    [ "rewind", "classcluon_1_1Player.html#ad90a5653fa321958ecd740e7cff06cfa", null ],
+    [ "seekTo", "classcluon_1_1Player.html#af69486732eff5488498259b394c47ef4", null ],
+    [ "setPlayerListener", "classcluon_1_1Player.html#ab9499699e2f2e74b4ef38634460b7de6", null ]
+];
