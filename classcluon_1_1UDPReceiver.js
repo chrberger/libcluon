c772860ae07000615fcf4033a6da9144722599fb
Christian Berger <christian.berger@gu.se>
2018-04-27 09:55:20 +0200

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- classcluon_1_1UDPReceiver.js
+++
@@ -1,6 +0,0 @@
-var classcluon_1_1UDPReceiver =
-[
-    [ "UDPReceiver", "classcluon_1_1UDPReceiver.html#a5be0d1c093f3a442724e8912c40773c8", null ],
-    [ "~UDPReceiver", "classcluon_1_1UDPReceiver.html#a41f1994fda9cecd280f6160b8b9482f2", null ],
-    [ "isRunning", "classcluon_1_1UDPReceiver.html#a8f371c1a0bb9325bab53d3464eb3828e", null ]
-];
