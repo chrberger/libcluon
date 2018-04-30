7b541ea24190aa5aa97566ce527b315330c6f59f
Christian Berger <christian.berger@gu.se>
2018-04-30 21:10:29 +0200

    * Two entries
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- libcluon/testsuites/TestPlayer.cpp
+++ libcluon/testsuites/TestPlayer.cpp
@@ -101,6 +101,76 @@ TEST_CASE("Create simple player for file with one entry.") {
 
         testdata::MyTestMessage5 msg = cluon::extractMessage<testdata::MyTestMessage5>(std::move(env));
         REQUIRE(retrievedEntries == msg.attribute5());
+
+        REQUIRE(0 == player.delay());
+    }
+    REQUIRE(MAX_ENTRIES == retrievedEntries);
+}
+
+TEST_CASE("Create simple player for file with two entries.") {
+    constexpr bool AUTO_REWIND{false};
+    constexpr bool THREADING{false};
+
+    constexpr uint32_t MAX_ENTRIES{2};
+    {
+        std::fstream recordingFile("rec2", std::ios::out|std::ios::binary|std::ios::trunc);
+        REQUIRE(recordingFile.good());
+
+        for(uint32_t entryCounter{0}; entryCounter < MAX_ENTRIES; entryCounter++) {
+            testdata::MyTestMessage5 msg;
+            msg.attribute5(entryCounter+1);
+
+            cluon::ToProtoVisitor proto;
+            msg.accept(proto);
+
+            cluon::data::Envelope env;
+            cluon::data::TimeStamp sent; sent.seconds(1000).microseconds(entryCounter);
+            cluon::data::TimeStamp received; received.seconds(5000).microseconds(entryCounter);
+            cluon::data::TimeStamp sampleTimeStamp; sampleTimeStamp.seconds(10000).microseconds(entryCounter);
+
+            env.serializedData(proto.encodedData());
+            env.dataType(testdata::MyTestMessage5::ID())
+               .sent(sent)
+               .received(received)
+               .sampleTimeStamp(sampleTimeStamp);
+
+            const std::string tmp{cluon::serializeEnvelope(std::move(env))};
+            recordingFile.write(tmp.c_str(), tmp.size());
+            recordingFile.flush();
+        }
+        recordingFile.close();
+    }
+    cluon::Player player("rec2", AUTO_REWIND, THREADING);
+
+    REQUIRE(player.hasMoreData());
+    REQUIRE(2 == player.totalNumberOfEnvelopesInRecFile());
+
+    uint32_t retrievedEntries{0};
+    while(player.hasMoreData()) {
+        auto entry = player.getNextEnvelopeToBeReplayed();
+        REQUIRE(entry.first);
+
+        cluon::data::Envelope env = entry.second;
+        REQUIRE(testdata::MyTestMessage5::ID() == env.dataType());
+
+        REQUIRE(1000 == env.sent().seconds());
+        REQUIRE(retrievedEntries == env.sent().microseconds());
+        REQUIRE(5000 == env.received().seconds());
+        REQUIRE(retrievedEntries == env.received().microseconds());
+        REQUIRE(10000 == env.sampleTimeStamp().seconds());
+        REQUIRE(retrievedEntries == env.sampleTimeStamp().microseconds());
+
+        retrievedEntries++;
+
+        testdata::MyTestMessage5 msg = cluon::extractMessage<testdata::MyTestMessage5>(std::move(env));
+        REQUIRE(retrievedEntries == msg.attribute5());
+
+        if (1 == retrievedEntries) {
+            REQUIRE(0 == player.delay());
+        }
+        else {
+            REQUIRE(1 == player.delay());
+        }
     }
     REQUIRE(MAX_ENTRIES == retrievedEntries);
 }
