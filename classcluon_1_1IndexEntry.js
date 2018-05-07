0c43608f5252b63ce6c5290074dbc6bb5a362043
Christian Berger <christian.berger@gu.se>
2018-05-07 07:40:39 +0200

    Documentation for v0.0.88
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ classcluon_1_1IndexEntry.js
@@ -0,0 +1,8 @@
+var classcluon_1_1IndexEntry =
+[
+    [ "IndexEntry", "classcluon_1_1IndexEntry.html#ac67cb9317b05aab65738dc3034f965d4", null ],
+    [ "IndexEntry", "classcluon_1_1IndexEntry.html#a9c79c606d1e5d298cb16ee1852235b98", null ],
+    [ "m_available", "classcluon_1_1IndexEntry.html#a0f766b273b6879edb204fbaf236967bd", null ],
+    [ "m_filePosition", "classcluon_1_1IndexEntry.html#a721bbdbd9796e42661bd896d32a27003", null ],
+    [ "m_sampleTimeStamp", "classcluon_1_1IndexEntry.html#a64e80ed5aa35ff27aa0856f080370016", null ]
+];
