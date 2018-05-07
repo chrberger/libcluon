ab74a1949c2af7d6d926e3c28533c621bc20cfb6
Christian Berger <christian.berger@gu.se>
2018-05-07 07:35:34 +0200

    * Testing using Player
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- libcluon/testsuites/Testcluon_OD4toStdout.cpp
+++ libcluon/testsuites/Testcluon_OD4toStdout.cpp
@@ -19,13 +19,24 @@
 
 #include "cluon-OD4toStdout.hpp"
 #include "cluon/cluonTestDataStructures.hpp"
+#include "cluon/Player.hpp"
 #include "cluon/TerminateHandler.hpp"
 
 #include <chrono>
+#include <fstream>
 #include <string>
 #include <sstream>
 #include <streambuf>
 #include <thread>
+
+// clang-format off
+#ifdef WIN32
+    #define UNLINK _unlink
+#else
+    #include <unistd.h>
+    #define UNLINK unlink
+#endif
+// clang-format on
 
 class RedirectCOUT {
    public:
@@ -60,6 +71,8 @@ TEST_CASE("Test wrong --cid.") {
 TEST_CASE("Test starting cluon-OD4toStdout in thread and send one message.") {
     // Reset TerminateHandler.
     cluon::TerminateHandler::instance().isTerminated.store(false);
+
+    UNLINK("GHI.rec");
 
     std::stringstream capturedCout;
     RedirectCOUT redirect(capturedCout.rdbuf());
@@ -103,85 +116,43 @@ TEST_CASE("Test starting cluon-OD4toStdout in thread and send one message.") {
 
     const std::string tmp = capturedCout.str();
 
-    // The size should be 107; we test only for the first 71 as the remainder is a changing time-stamp.
-    REQUIRE(71 <= tmp.size());
+    // Write received data into file and replay with Player.
+    {
+        std::fstream fout("GHI.rec", std::ios::out|std::ios::binary|std::ios::trunc);
+        fout << tmp;
+        fout.flush();
+        fout.close();
 
-    REQUIRE(0xd == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(0))));
-    REQUIRE(0xa4 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(1))));
-    REQUIRE(0x66 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(2))));
-    REQUIRE(0x0 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(3))));
-    REQUIRE(0x0 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(4))));
-    REQUIRE(0x8 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(5))));
-    REQUIRE(0xea == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(6))));
-    REQUIRE(0xd4 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(7))));
-    REQUIRE(0x3 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(8))));
-    REQUIRE(0x12 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(9))));
-    REQUIRE(0x3a == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(10))));
-    REQUIRE(0x8 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(11))));
-    REQUIRE(0x3 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(12))));
-    REQUIRE(0x10 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(13))));
-    REQUIRE(0x5 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(14))));
-    REQUIRE(0x18 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(15))));
-    REQUIRE(0x67 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(16))));
-    REQUIRE(0x20 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(17))));
-    REQUIRE(0xcd == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(18))));
-    REQUIRE(0x1 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(19))));
-    REQUIRE(0x28 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(20))));
-    REQUIRE(0x93 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(21))));
-    REQUIRE(0x4e == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(22))));
-    REQUIRE(0x30 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(23))));
-    REQUIRE(0xa5 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(24))));
-    REQUIRE(0x9c == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(25))));
-    REQUIRE(0x1 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(26))));
-    REQUIRE(0x38 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(27))));
-    REQUIRE(0xb1 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(28))));
-    REQUIRE(0xa8 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(29))));
-    REQUIRE(0x3 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(30))));
-    REQUIRE(0x40 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(31))));
-    REQUIRE(0xa1 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(32))));
-    REQUIRE(0x89 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(33))));
-    REQUIRE(0x9 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(34))));
-    REQUIRE(0x4d == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(35))));
-    REQUIRE(0xc3 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(36))));
-    REQUIRE(0xd3 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(37))));
-    REQUIRE(0xad == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(38))));
-    REQUIRE(0xc0 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(39))));
-    REQUIRE(0x51 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(40))));
-    REQUIRE(0x1 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(41))));
-    REQUIRE(0x4d == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(42))));
-    REQUIRE(0x84 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(43))));
-    REQUIRE(0xd == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(44))));
-    REQUIRE(0x4f == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(45))));
-    REQUIRE(0x37 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(46))));
-    REQUIRE(0x49 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(47))));
-    REQUIRE(0xc0 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(48))));
-    REQUIRE(0x5a == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(49))));
-    REQUIRE(0x12 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(50))));
-    REQUIRE(0x48 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(51))));
-    REQUIRE(0x65 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(52))));
-    REQUIRE(0x6c == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(53))));
-    REQUIRE(0x6c == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(54))));
-    REQUIRE(0x6f == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(55))));
-    REQUIRE(0x20 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(56))));
-    REQUIRE(0x63 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(57))));
-    REQUIRE(0x6c == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(58))));
-    REQUIRE(0x75 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(59))));
-    REQUIRE(0x6f == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(60))));
-    REQUIRE(0x6e == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(61))));
-    REQUIRE(0x20 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(62))));
-    REQUIRE(0x57 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(63))));
-    REQUIRE(0x6f == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(64))));
-    REQUIRE(0x72 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(65))));
-    REQUIRE(0x6c == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(66))));
-    REQUIRE(0x64 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(67))));
-    REQUIRE(0x21 == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(68))));
-    REQUIRE(0x1a == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(69))));
-    REQUIRE(0xa == static_cast<uint32_t>(static_cast<uint8_t>(tmp.at(70))));
+        constexpr bool AUTO_REWIND{false};
+        constexpr bool THREADING{false};
+        cluon::Player player("GHI.rec", AUTO_REWIND, THREADING);
 
-    // Remaining bytes (71-106) cover timestamps.
+        REQUIRE(player.hasMoreData());
+        REQUIRE(1 == player.totalNumberOfEnvelopesInRecFile());
+
+        auto entry = player.getNextEnvelopeToBeReplayed();
+        REQUIRE(entry.first);
+
+        cluon::data::Envelope env = entry.second;
+        REQUIRE(testdata::MyTestMessage5::ID() == env.dataType());
+
+        testdata::MyTestMessage5 tmp2 = cluon::extractMessage<testdata::MyTestMessage5>(std::move(env));
+        REQUIRE(3 == tmp2.attribute1());
+        REQUIRE(-3 == tmp2.attribute2());
+        REQUIRE(103 == tmp2.attribute3());
+        REQUIRE(-103 == tmp2.attribute4());
+        REQUIRE(10003 == tmp2.attribute5());
+        REQUIRE(-10003 == tmp2.attribute6());
+        REQUIRE(54321 == tmp2.attribute7());
+        REQUIRE(-74321 == tmp2.attribute8());
+        REQUIRE(-5.4321f == Approx(tmp2.attribute9()));
+        REQUIRE(-50.4321 == Approx(tmp2.attribute10()));
+        REQUIRE("Hello cluon World!" == tmp2.attribute11());
+    }
 
     cluon::TerminateHandler::instance().isTerminated.store(true);
 
     runOD4toStdout.join();
+    UNLINK("GHI.rec");
 }
 
