563983fbd23a4801f30d7638618c36475e2fb208
Christian Berger <christian.berger@gu.se>
2018-05-13 21:30:40 +0200

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- TerminateHandler_8cpp.js
+++
@@ -1,5 +0,0 @@
-var TerminateHandler_8cpp =
-[
-    [ "cluon_handleExit", "TerminateHandler_8cpp.html#a5af169f11066223d295d7830911fd133", null ],
-    [ "cluon_handleSignal", "TerminateHandler_8cpp.html#ada3629371549558f74bb89678696cd00", null ]
-];
