2e5634acf0742d4b3104407db0cb22912fe6311f
Christian Berger <christian.berger@gu.se>
2018-07-09 11:29:04 +0200

    Documentation for v0.0.103
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ classcluon_1_1EnvelopeConverter.js
@@ -0,0 +1,8 @@
+var classcluon_1_1EnvelopeConverter =
+[
+    [ "EnvelopeConverter", "classcluon_1_1EnvelopeConverter.html#a5e8ece063bd077c98698288eb4d49b65", null ],
+    [ "getJSONFromEnvelope", "classcluon_1_1EnvelopeConverter.html#a8d7438e6f54c7d4034688634a19ece83", null ],
+    [ "getJSONFromProtoEncodedEnvelope", "classcluon_1_1EnvelopeConverter.html#adb3484e4e1336792ffb51d870e0f8028", null ],
+    [ "getProtoEncodedEnvelopeFromJSONWithoutTimeStamps", "classcluon_1_1EnvelopeConverter.html#ab102d020ec1701c9e6b545f05f4dc6c8", null ],
+    [ "setMessageSpecification", "classcluon_1_1EnvelopeConverter.html#af4879d33814e727df61273fdd4ed973b", null ]
+];
