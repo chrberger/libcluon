abca88e3fb8c445f7f6a1d654dbb89930c11df99
Christian Berger <christian.berger@gu.se>
2018-09-22 19:50:17 +0200

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- MetaMessageToCPPTransformator_8cpp.js
+++
@@ -1,4 +0,0 @@
-var MetaMessageToCPPTransformator_8cpp =
-[
-    [ "headerFileTemplate", "MetaMessageToCPPTransformator_8cpp.html#a330e3f48aeb2dae523125f7135c8dad1", null ]
-];
--- classcluon_1_1OD4Session.js
+++
@@ -1,9 +0,0 @@
-var classcluon_1_1OD4Session =
-[
-    [ "OD4Session", "classcluon_1_1OD4Session.html#ad9d26426cf2714e105c27a23ce4a0f7a", null ],
-    [ "dataTrigger", "classcluon_1_1OD4Session.html#a5d79b313afe89bc3a8622d1801c0037c", null ],
-    [ "isRunning", "classcluon_1_1OD4Session.html#a1381dc975919e3ef0592e9bfa032c8df", null ],
-    [ "send", "classcluon_1_1OD4Session.html#a8d9843ae35fa1a0fbe130877aa959025", null ],
-    [ "send", "classcluon_1_1OD4Session.html#a01745bfed160ac4fd16c71828672e7e8", null ],
-    [ "timeTrigger", "classcluon_1_1OD4Session.html#a2bd324b893072614ba917e5f1fd1cdc2", null ]
-];
