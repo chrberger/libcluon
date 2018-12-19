1b9863ab2d29315d8499a17f15707e1363b01e27
Christian Berger <christian.berger@gu.se>
2018-12-19 06:48:38 +0100

    Documentation for v0.0.116
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ classcluon_1_1TCPServer.js
@@ -0,0 +1,6 @@
+var classcluon_1_1TCPServer =
+[
+    [ "TCPServer", "classcluon_1_1TCPServer.html#ab4687285c5779ff7135f388f81d90f50", null ],
+    [ "~TCPServer", "classcluon_1_1TCPServer.html#a33d7e1eeb83b39fa4db9533a25de817c", null ],
+    [ "isRunning", "classcluon_1_1TCPServer.html#a8a984878e7f1c210b101fca0caa1ee3e", null ]
+];
---
+++ classcluon_1_1UDPSender.js
@@ -0,0 +1,7 @@
+var classcluon_1_1UDPSender =
+[
+    [ "UDPSender", "classcluon_1_1UDPSender.html#a7f0b38364eb6dda1a95eb297139108b0", null ],
+    [ "~UDPSender", "classcluon_1_1UDPSender.html#a7f76b9993ca6156faafad3674941968b", null ],
+    [ "getSendFromPort", "classcluon_1_1UDPSender.html#a769652a66b5394452956678ca34df018", null ],
+    [ "send", "classcluon_1_1UDPSender.html#ab3bf8ad9ac686a3b5bea5ecc74e7e8e1", null ]
+];
