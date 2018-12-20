05bbf52b2e6eb4e90b32e433831e2de52d43993b
Christian Berger <christian.berger@gu.se>
2018-12-20 15:19:06 +0100

    * Linux using /dev/shm/... again
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- libcluon/src/SharedMemory.cpp
+++ libcluon/src/SharedMemory.cpp
@@ -579,20 +579,19 @@ void SharedMemory::initPOSIX() noexcept {
     }
 #endif
 
-//#ifdef __linux__
-//    // On Linux, the POSIX shared memory lives in /dev/shm and we have a valid
-//    // file descriptor to use for timestamping.
-//    if (-1 != m_fd) {
-//        m_fdForTimeStamping = m_fd;
-//    }
-//#else
+#ifdef __linux__
+    // On Linux, the POSIX shared memory lives in /dev/shm and we have a valid
+    // file descriptor to use for timestamping.
+    if (-1 != m_fd) {
+        m_fdForTimeStamping = m_fd;
+    }
+#else
     // On *BSDs, the POSIX shared memory lives not in /dev/shm and we have
     // need to use a separate file for timestamping.
     if (-1 != m_fd) {
-std::cout << "POSIX Opening '" << m_nameForTimeStamping << "'" << std::endl;
         m_fdForTimeStamping = ::open(m_nameForTimeStamping.c_str(), O_CREAT|O_RDONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     }
-//#endif
+#endif
 }
 
 void SharedMemory::deinitPOSIX() noexcept {
@@ -615,14 +614,14 @@ void SharedMemory::deinitPOSIX() noexcept {
     }
 #endif
 
-//#ifndef __linux__
+#ifndef __linux__
     // On *BSDs, the POSIX shared memory lives not in /dev/shm and we have
     // used a separate file for timestamping.
     if (-1 != m_fdForTimeStamping) {
         ::close(m_fdForTimeStamping);
         ::unlink(m_nameForTimeStamping.c_str());
     }
-//#endif
+#endif
 }
 
 void SharedMemory::lockPOSIX() noexcept {
@@ -916,7 +915,6 @@ void SharedMemory::initSysV() noexcept {
 
     // If the shared memory is present, open the token file for the time stamping.
     if (nullptr != m_sharedMemory) {
-std::cout << "SysV Opening '" << m_nameForTimeStamping << "'" << std::endl;
         m_fdForTimeStamping = ::open(m_name.c_str(), O_RDONLY);
     }
 }
