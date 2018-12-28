e358b60b7af2a1cd43c8b7f2f3f2dc93fd3d8e68
Christian Berger <christian.berger@gu.se>
2018-12-28 21:22:25 +0100

    Documentation for v0.0.120
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ classcluon_1_1TCPConnection.js
@@ -0,0 +1,10 @@
+var classcluon_1_1TCPConnection =
+[
+    [ "TCPConnection", "classcluon_1_1TCPConnection.html#a944df3d518fbbc5fc462d522d30a0cd3", null ],
+    [ "~TCPConnection", "classcluon_1_1TCPConnection.html#a1592d281a7b873046c1d22d3e17e00c8", null ],
+    [ "isRunning", "classcluon_1_1TCPConnection.html#a668b63d0a93c03fa7ead086d8fe80c66", null ],
+    [ "send", "classcluon_1_1TCPConnection.html#a5e7e72bd00d40f107846bbcdcf5eb07f", null ],
+    [ "setOnConnectionLost", "classcluon_1_1TCPConnection.html#a81e2b8456ee8eb29abf0836e33022ce5", null ],
+    [ "setOnNewData", "classcluon_1_1TCPConnection.html#a4a206cba4b0a91019d30f2f371da437c", null ],
+    [ "TCPServer", "classcluon_1_1TCPConnection.html#ae4cfdb1814d91a8d28dadb49adda68f0", null ]
+];
