69a9c40b4eaccce4f8139517f85904c849159311
Christian Berger <christian.berger@gu.se>
2019-08-03 15:58:45 +0200

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- UDPPacketSizeConstraints_8hpp.js
+++
@@ -1,8 +0,0 @@
-var UDPPacketSizeConstraints_8hpp =
-[
-    [ "UDPPacketSizeConstraints", "UDPPacketSizeConstraints_8hpp.html#ae13fc5af4d0565408b5487cf0d6c0910", [
-      [ "SIZE_IPv4_HEADER", "UDPPacketSizeConstraints_8hpp.html#ae13fc5af4d0565408b5487cf0d6c0910ad2a234bb6faedeef2be5adfc03f195a4", null ],
-      [ "SIZE_UDP_HEADER", "UDPPacketSizeConstraints_8hpp.html#ae13fc5af4d0565408b5487cf0d6c0910a840a6d5ecacba6823a0523c7c27182db", null ],
-      [ "MAX_SIZE_UDP_PACKET", "UDPPacketSizeConstraints_8hpp.html#ae13fc5af4d0565408b5487cf0d6c0910a624f828bf1caf71cb881443a02f3379e", null ]
-    ] ]
-];
