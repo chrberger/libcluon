61f334849a5f52d08c2561a387917c1682191070
Christian Berger <christian.berger@gu.se>
2019-08-10 00:00:19 +0200

    Documentation for v0.0.123
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ JSONConstants_8hpp.js
@@ -0,0 +1,11 @@
+var JSONConstants_8hpp =
+[
+    [ "JSONConstants", "JSONConstants_8hpp.html#a2cd932e6cf509c818cfb58fbe9b46899", [
+      [ "IS_FALSE", "JSONConstants_8hpp.html#a2cd932e6cf509c818cfb58fbe9b46899adeab83d56dee7ab418359a59b4ff2eb0", null ],
+      [ "IS_TRUE", "JSONConstants_8hpp.html#a2cd932e6cf509c818cfb58fbe9b46899a23cd5ba5c6eed3d82ba0c6c0526df429", null ],
+      [ "NUMBER", "JSONConstants_8hpp.html#a2cd932e6cf509c818cfb58fbe9b46899a34f55eca38e0605a84f169ff61a2a396", null ],
+      [ "STRING", "JSONConstants_8hpp.html#a2cd932e6cf509c818cfb58fbe9b46899a63b588d5559f64f89a416e656880b949", null ],
+      [ "OBJECT", "JSONConstants_8hpp.html#a2cd932e6cf509c818cfb58fbe9b46899a8eee8e217391199668cbac89472ace53", null ],
+      [ "UNDEFINED", "JSONConstants_8hpp.html#a2cd932e6cf509c818cfb58fbe9b46899a0db45d2a4141101bdfe48e3314cfbca3", null ]
+    ] ]
+];
