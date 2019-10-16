959e99dbf004e3a508ea3bfcac3ffbd456bfbe21
Christian Berger <christian.berger@gu.se>
2019-10-16 22:13:30 +0200

    Update CompileFlags.cmake

--- buildtools/cmake/CompileFlags.cmake
+++ buildtools/cmake/CompileFlags.cmake
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
