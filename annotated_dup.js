15fa476ec84e4bbffbf3e8db9852cae95bf96647
Christian Berger <christian.berger@gu.se>
2019-10-16 07:13:04 +0200

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- annotated_dup.js
+++
@@ -1,7 +0,0 @@
-var annotated_dup =
-[
-    [ "cluon", "namespacecluon.html", "namespacecluon" ],
-    [ "isTripletForwardVisitable< cluon::GenericMessage >", "structisTripletForwardVisitable_3_01cluon_1_1GenericMessage_01_4.html", "structisTripletForwardVisitable_3_01cluon_1_1GenericMessage_01_4" ],
-    [ "isVisitable< cluon::GenericMessage >", "structisVisitable_3_01cluon_1_1GenericMessage_01_4.html", "structisVisitable_3_01cluon_1_1GenericMessage_01_4" ],
-    [ "semun", "unionsemun.html", "unionsemun" ]
-];
--- structisVisitable_3_01cluon_1_1GenericMessage_01_4.js
+++
@@ -1,4 +0,0 @@
-var structisVisitable_3_01cluon_1_1GenericMessage_01_4 =
-[
-    [ "value", "structisVisitable_3_01cluon_1_1GenericMessage_01_4.html#a95a7f89d20e16352e0063716b95147da", null ]
-];
