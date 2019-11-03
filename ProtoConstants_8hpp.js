b32f4f3610a267ea0a572799ad9d21a3502f9c59
Christian Berger <christian.berger@gu.se>
2019-11-03 15:21:05 +0100

    Cleaning
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- ProtoConstants_8hpp.js
+++
@@ -1,9 +0,0 @@
-var ProtoConstants_8hpp =
-[
-    [ "ProtoConstants", "ProtoConstants_8hpp.html#adfd4830da53be3e410eb1520625f69d3", [
-      [ "VARINT", "ProtoConstants_8hpp.html#adfd4830da53be3e410eb1520625f69d3ad1441bc7cefd4cf4dc1282282ae5c529", null ],
-      [ "EIGHT_BYTES", "ProtoConstants_8hpp.html#adfd4830da53be3e410eb1520625f69d3af0a9f7a988579d6eb0c767da729e578c", null ],
-      [ "LENGTH_DELIMITED", "ProtoConstants_8hpp.html#adfd4830da53be3e410eb1520625f69d3ae531c39d69190463103ef1bba9085614", null ],
-      [ "FOUR_BYTES", "ProtoConstants_8hpp.html#adfd4830da53be3e410eb1520625f69d3aece10bb19544946080220406b51cd56b", null ]
-    ] ]
-];
