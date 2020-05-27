32115e38e9d250cd32c0608e591e1a96dd240a95
Christian Berger <christian.berger@gu.se>
2020-05-27 17:22:59 +0200

    Documentation for v0.0.131
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ classcluon_1_1NotifyingPipeline.js
@@ -0,0 +1,8 @@
+var classcluon_1_1NotifyingPipeline =
+[
+    [ "NotifyingPipeline", "classcluon_1_1NotifyingPipeline.html#af2d26ae34b6de2ca4eebc7dee60da1a9", null ],
+    [ "~NotifyingPipeline", "classcluon_1_1NotifyingPipeline.html#a0c41fcb72d3711ec73516fa4b1764be4", null ],
+    [ "add", "classcluon_1_1NotifyingPipeline.html#aab5571d4b60e27fa4cead10a42571239", null ],
+    [ "isRunning", "classcluon_1_1NotifyingPipeline.html#ae53ad7a8cd33a0bda4645297058ca7a8", null ],
+    [ "notifyAll", "classcluon_1_1NotifyingPipeline.html#aa7854471e47b467a83e163ee22ea710b", null ]
+];
